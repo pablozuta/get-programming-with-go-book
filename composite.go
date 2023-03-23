9ea79b7c264dd1b4d37f5118d85fa742ba621642
pablozuta <pablo.zuta.h@gmail.com>
2023-03-23 18:26:48 -0400

    se agrega modulo 'composite'

---
+++ composite.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	planets := [...]string {
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune",
+	}
+	fmt.Println(planets)
+
+	books := [...]string {
+		"Infinite Jest",
+		"White Noise",
+		"Gravity's Rainbow",
+		"The Recognitions",
+		"UnderWorld",
+		"Mason & Dixon",
+		"The Crying of Lot 49",
+	}
+	fmt.Println(books)
+ 	
+}
